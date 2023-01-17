a39e7724e70d5bf2bf70068a2b08c860217c3880
KingfernJohn <49335210+KingfernJohn@users.noreply.github.com>
2023-01-17 22:35:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+#########################
+# Replace with your GitHub username
+username = "KingfernJohn"
+#########################
+
+response = requests.get(f"https://api.github.com/users/{username}/repos")
+data = json.loads(response.text)
+formatted_data = {"username": username, "repositories": []}
+
+for repo in data:
+    repository = {
+        "name": repo["name"],
+        "stars": repo["stargazers_count"],
+        "license": repo["license"]["name"] if repo["license"] else None,
+        "language": repo["language"]
+    }
+    formatted_data["repositories"].append(repository)
+
+json_data = json.dumps(formatted_data, indent=4)
+with open(f"{username}-gitdata.json", "w") as f:
+    f.write(json_data)
+print(json_data)
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off 
+color 02 
+PYTHON main.py 
+PAUSE 
